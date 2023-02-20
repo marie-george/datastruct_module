a5029deb1d9bfe9503a7535e2b181c4c7d71ad5d
marie-george <123417875+marie-george@users.noreply.github.com>
2023-02-21 00:00:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,18 +16,3 @@ class Stack:
         self.head = new_node
 
 
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next_node)
-
-stack = Stack()
-stack.push('data1')
-stack.push('data2')
-stack.push('data3')
-print(stack.head.data)
-print(stack.head.next_node.data)
-print(stack.head.next_node.next_node.data)
-print(stack.head.next_node.next_node.next_node.data)
