e22a9020bc7f94d83a331556d8c9d7bf4b444a48
marie-george <maria85night@gmail.com>
2023-02-24 04:10:10 +0300

    pop added

--- main.py
+++ main.py
@@ -15,4 +15,38 @@ class Stack:
         new_node.next_node = self.head
         self.head = new_node
 
+    def pop(self):
+        if self.head is None:
+            return
+        value = self.head.data
+        self.head = self.head.next_node
+        return value
+
+
+# stack = Stack()
+# stack.push('data1')
+# data = stack.pop()
+#
+# # стэк стал пустой
+# print(stack.head)
+#
+# # # pop() удаляет элемент и возвращает данные удаленного элемента
+# print(data)
+#
+# stack = Stack()
+# stack.push('data1')
+# stack.push('data2')
+# data = stack.pop()
+#
+# # теперь последний элемента содержит данные data1
+# print(stack.head.data)
+#
+#
+# # данные удаленного элемента
+# print(data)
+# 'data2'
+
+
+
+
 
