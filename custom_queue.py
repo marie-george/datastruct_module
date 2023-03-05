62e59ff58698aabe8cbded97ac220fd2976ad934
marie-george <maria85night@gmail.com>
2023-03-05 15:44:10 +0300

    tests for dequeue function

--- custom_queue.py
+++ custom_queue.py
@@ -19,14 +19,20 @@ class Queue:
             self.tail.next_node = new_node
             self.tail = new_node
 
+    def dequeue(self):
+        if self.head is None:
+            return
+        value = self.head.data
+        self.head = self.head.next_node
+        return value
+
 # queue = Queue()
 # queue.enqueue('data1')
 # queue.enqueue('data2')
 # queue.enqueue('data3')
-#
-# print(queue.head.data)
-# print(queue.head.next_node.data)
-# print(queue.tail.data)
-# print(queue.tail.next_node)
-# print(queue.tail.next_node.data)
+# print(queue.dequeue())
+# print(queue.dequeue())
+# print(queue.dequeue())
+# print(queue.dequeue())
+
 
